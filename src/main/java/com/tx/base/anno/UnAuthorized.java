43741d575012433971142ced482e49dc157031b3
小菜鸟 <67440@163.com>
2021-12-02 18:11:49 +0800

    增加jwt相关方法

---
+++ src/main/java/com/tx/base/anno/UnAuthorized.java
@@ -0,0 +1,16 @@
+package com.tx.base.anno;
+
+import java.lang.annotation.*;
+
+/**
+ * @description: --加此注解的方法，不需要验证授权。用到controller的方法上
+ * @author：Bing
+ * @date：2021/12/2 18:05
+ * @version：1.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface UnAuthorized {
+    boolean required();
+}
