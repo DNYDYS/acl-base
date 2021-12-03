1a55d8a21545c7fa5079709a9fa564b791d8f31c
小菜鸟 <4065678@gmail.com>
2021-12-03 10:51:49 +0800

    添加地区

---
+++ src/main/java/com/tx/base/primary/mapper/YmRegionMapper.java
@@ -0,0 +1,15 @@
+package com.tx.base.primary.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tx.base.primary.entity.YmRegion;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @description: --
+ * @author：Bing
+ * @date：2021/12/3 10:43
+ * @version：1.0
+ */
+@Mapper
+public interface YmRegionMapper extends BaseMapper<YmRegion> {
+}
