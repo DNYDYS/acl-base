c01b06d88a2fe744976bcc79bd904fb50eb80bed
小菜鸟 <4065678@gmail.com>
2021-12-03 17:15:26 +0800

    添加说明文档

--- src/main/java/com/tx/base/handler/MyMetaObjectHandler.java
+++ src/main/java/com/tx/base/handler/MyMetaObjectHandler.java
@@ -9,6 +9,8 @@ import java.util.Date;
 /**
  * 前端传值添加默认值
  * 给对象添加默认值
+ * MetaObjectHandler接口是mybatisPlus为我们提供的的一个扩展接口，我们可以利用这个接口在我们插入或者更新数据的时候，
+ * 为一些字段指定默认值。实现这个需求的方法不止一种，在sql层面也可以做到，在建表的时候也可以指定默认值。
  */
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
