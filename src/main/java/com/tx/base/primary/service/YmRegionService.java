36488755156056161aa63e964f3ece4502cd3b2a
小菜鸟 <4065678@gmail.com>
2021-12-03 10:52:19 +0800

    添加地区

---
+++ src/main/java/com/tx/base/primary/service/YmRegionService.java
@@ -0,0 +1,14 @@
+package com.tx.base.primary.service;
+
+import com.tx.base.primary.entity.YmRegion;
+import com.baomidou.mybatisplus.extension.service.IService;
+    /**
+ * @description: --
+ * @author：Bing
+ * @date：2021/12/3 10:43
+ * @version：1.0
+ */
+public interface YmRegionService extends IService<YmRegion>{
+
+
+}
