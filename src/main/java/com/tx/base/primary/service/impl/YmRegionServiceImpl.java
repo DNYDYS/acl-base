4d9b807a7892ca6c28295870d80598701bf9ed93
小菜鸟 <4065678@gmail.com>
2021-12-03 10:52:10 +0800

    添加地区

---
+++ src/main/java/com/tx/base/primary/service/impl/YmRegionServiceImpl.java
@@ -0,0 +1,19 @@
+package com.tx.base.primary.service.impl;
+
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tx.base.primary.entity.YmRegion;
+import com.tx.base.primary.mapper.YmRegionMapper;
+import com.tx.base.primary.service.YmRegionService;
+/**
+ * @description: --
+ * @author：Bing
+ * @date：2021/12/3 10:43
+ * @version：1.0
+ */
+@Service
+public class YmRegionServiceImpl extends ServiceImpl<YmRegionMapper, YmRegion> implements YmRegionService{
+
+}
