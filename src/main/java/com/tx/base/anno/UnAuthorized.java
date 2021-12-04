0c9b92826a047d4886c6a8a677ca08914100e76d
小菜鸟 <4065678@gmail.com>
2021-12-04 15:00:38 +0800

    自定义注解

--- src/main/java/com/tx/base/anno/UnAuthorized.java
+++ src/main/java/com/tx/base/anno/UnAuthorized.java
@@ -3,7 +3,7 @@ package com.tx.base.anno;
 import java.lang.annotation.*;
 
 /**
- * @description: --加此注解的方法，不需要验证授权。用到controller的方法上
+ * @description: --自定义注解，加此注解的方法，不需要验证授权。用到controller的方法上
  * @author：Bing
  * @date：2021/12/2 18:05
  * @version：1.0
--- src/main/java/com/tx/base/handler/MyMetaObjectHandler.java
+++ src/main/java/com/tx/base/handler/MyMetaObjectHandler.java
@@ -8,9 +8,12 @@ import java.util.Date;
 
 /**
  * 前端传值添加默认值
- * 给对象添加默认值
+ * 给对象属性添加默认值
  * MetaObjectHandler接口是mybatisPlus为我们提供的的一个扩展接口，我们可以利用这个接口在我们插入或者更新数据的时候，
  * 为一些字段指定默认值。实现这个需求的方法不止一种，在sql层面也可以做到，在建表的时候也可以指定默认值。
+ * 用法：
+ * 1. 编写MetaObjectHandler 实现类
+ * 2. 实体类上边加上@TableField(fill = FieldFill.INSERT_UPDATE)
  */
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
